ad11aa2c6d854d97a29260135d52cabc5e358114
Damian <damianmarti@gmail.com>
2023-03-16 19:04:25 -0300

    Remove unused bytes32ToString function

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -92,29 +92,6 @@ contract YourContract is ERC20 {
         _mint(msg.sender, 1 ether);
     }
 
-    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
-        uint8 i = 0;
-        bytes memory bytesArray = new bytes(64);
-        for (i = 0; i < bytesArray.length; i++) {
-
-            uint8 _f = uint8(_bytes32[i/2] & 0x0f);
-            uint8 _l = uint8(_bytes32[i/2] >> 4);
-
-            bytesArray[i] = toByte(_f);
-            i = i + 1;
-            bytesArray[i] = toByte(_l);
-        }
-        return string(bytesArray);
-    }
-
-    function toByte(uint8 _uint8) public pure returns (bytes1) {
-        if(_uint8 < 10) {
-            return bytes1(_uint8 + 48);
-        } else {
-            return bytes1(_uint8 + 87);
-        }
-    }
-
     function setCommit(bytes32 commitHash) public {
         require(gameOn, "Game is not on");
         require(!openToCollect, "Round has ended");
